915bb21077a00664d54cdce47c8f04fb2b81995f
Chris Morrell <inxilpro@users.noreply.github.com>
2021-10-11 21:02:42 -0400

    Pest

--- tests/DeferredCollectionTest.php
+++ tests/DeferredCollectionTest.php
@@ -1,16 +1,12 @@
 <?php
 
-namespace Glhd\Suspend\Tests;
-
-class DeferredCollectionTest extends TestCase
-{
-	public function test_it_applies_basic_mapping_and_filtering_operations(): void
-	{
-		$result = suspend([1, 2, 3, 4])
-			->filter(fn($value) => 0 === $value % 2)
-			->map(fn($value) => $value * 10)
-			->toArray();
-		
-		$this->assertSame([20, 40], $result);
-	}
-}
+test('basic mapping and filtering', function() {
+	$result = suspend([1, 2, 3, 4])
+		->filter(fn($value) => 0 === $value % 2)
+		->map(fn($value) => $value * 10)
+		->filter(fn($value) => $value === 40)
+		->map(fn($value) => $value * 10)
+		->toArray();
+	
+	expect($result)->toEqual([400]);
+});
